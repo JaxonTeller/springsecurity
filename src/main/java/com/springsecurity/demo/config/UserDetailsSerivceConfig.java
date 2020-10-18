095dcc6fa2a42ff3543862cbbc3cf4688f7929f8
JaxonTeller <nishuzephyrus@outlook.com>
2020-10-18 13:18:13 +0530

    Craeting PasswordEncoder bean

--- src/main/java/com/springsecurity/demo/config/UserDetailsSerivceConfig.java
+++ src/main/java/com/springsecurity/demo/config/UserDetailsSerivceConfig.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @RequiredArgsConstructor
-public class UserDetailsSerivceConfig {
+public class UserDetailsSerivceConfig extends WebSecurityConfigurerAdapter {
 
     /*This will autowire the UserDetailsConfig*/
     private final UserDetails userDetails;
@@ -30,5 +34,11 @@ public class UserDetailsSerivceConfig {
         UserDetails userDetails = User.withUsername("User").password("Password").authorities("READ").build();
         var userDetailsService=new InMemoryUserDetailsManager(this.userDetails);
         return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
