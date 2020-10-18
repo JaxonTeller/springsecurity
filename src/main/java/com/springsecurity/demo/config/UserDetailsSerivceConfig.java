f10a65540eef26510b897e4b40c6d2cd7a5ee2c3
JaxonTeller <nishuzephyrus@outlook.com>
2020-10-18 13:33:52 +0530

    Extending WebMvcConfigAdaptor for overriding the authorization

--- src/main/java/com/springsecurity/demo/config/UserDetailsSerivceConfig.java
+++ src/main/java/com/springsecurity/demo/config/UserDetailsSerivceConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -40,5 +41,11 @@ public class UserDetailsSerivceConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getPasswordEncoder()
     {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
